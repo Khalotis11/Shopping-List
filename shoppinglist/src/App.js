8a396e90efaa78f01006f9c454217b2bc0151c5e
Khalotis11 <moekhaloti@gmail.com>
2019-06-11 01:44:09 +0100

    Added pay icons to Pay component

--- shoppinglist/src/App.js
+++ shoppinglist/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import logo from "./logo.jpg";
 import "./App.css";
 
-import ShoppingList from "./components/ShoppingList"
+import ShoppingList from "./components/ShoppingList";
+import Pay from "./components/Pay";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Pay />
         <ShoppingList />
       </header>
       
---
+++ shoppinglist/src/components/Pay.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Pay = () => {
+  return (
+    <div className="pay">
+      <i class="fab fa-cc-paypal" />
+      <i class="fab fa-apple-pay" />
+      <i class="fab fa-cc-discover" />
+      <i class="fab fa-amazon-pay" />
+    </div>
+  );
+};
+export default Pay;
